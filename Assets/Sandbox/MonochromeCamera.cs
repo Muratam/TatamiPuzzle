7337838c20a94cf68bed8021847a438cf0e44cc4
murata <murata@kmc.gr.jp>
2016-11-09 15:24:04 +0900

    for prerelease

---
+++ Assets/Sandbox/MonochromeCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class MonochromeCamera : MonoBehaviour {
+
+    [SerializeField] Shader monochromeShader;
+
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.D)) {
+            DoMonochrome();
+        } else if(Input.GetKeyDown(KeyCode.U)) {
+            UndoMonochrome();
+        }
+    }
+
+
+    public void DoMonochrome() {
+        GetComponent<Camera>().SetReplacementShader(monochromeShader, null);
+        Debug.Log("D");
+    }
+
+    public void UndoMonochrome() {
+        GetComponent<Camera>().SetReplacementShader(null, null);
+        Debug.Log("U");
+    }
+
+
+}
